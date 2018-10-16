3412a1486a6a40eb6cd7629930322d02481daff1
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-10-16 15:33:20 -0700

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    to: 'alice',
-    text: 'hello!!'
-  });
 });
 
 socket.on('disconnect', function() {
