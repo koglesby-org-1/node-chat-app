47804cb33fdd31816db202e07b54493477e1fe76
Kevin Oglesby <kevinjoglesby@gmail.com>
2018-10-23 11:46:16 -0700

    sent messages to only people on same room

--- server/server.js
+++ server/server.js
@@ -43,16 +43,23 @@ io.on('connection', socket => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
+    const user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit(
+        'newMessage',
+        generateMessage(user.name, message.text)
+      );
+    }
+
     callback();
   });
 
   socket.on('createLocationMessage', coords => {
-    io.emit(
+    const user = users.getUser(socket.id);
+    io.to(user.room).emit(
       'newLocationMessage',
-      generateLocationMessage('Admin', coords.latitude, coords.longitude)
+      generateLocationMessage(user.name, coords.latitude, coords.longitude)
     );
   });
 
